35c77c8b5d979bd179c87a586f05aa8739f93cc6
Gabriellji <gabriellji@yahoo.com>
2021-01-27 12:38:52 +0300

    feat: connect sql db

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,14 @@
 require('dotenv').config()
 
 const express = require('express')
+const db = require('./database/config')
 
 const app = express()
+
+db.connector
+    .sync()
+    .then(() => console.log('find current DB'))
+    .catch((err) => console.error(`sync faild: ${err}`))
 
 app.use((req, res, next) => {
     let err = new Error('Not found')
