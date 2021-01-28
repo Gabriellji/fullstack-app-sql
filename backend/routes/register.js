cc0e511165b6a508ddb2e847c208591416f30e7e
Gabriellji <gabriellji@yahoo.com>
2021-01-28 11:50:57 +0300

    feat: implement register route

--- backend/routes/register.js
+++ backend/routes/register.js
@@ -1,13 +1,21 @@
 const express = require('express')
-const app = require('../app')
 const router = express.Router()
 const bcrypt = require('bcrypt')
+const db = require('../database/config')
+const User = db.user
 
 router.post('/', (req, res) => {
     bcrypt
         .hash(req.body.password, 10)
         .then((hashedPass) => {
-            console.log(hashedPass)
+            User.create({
+                firstname: req.body.firstname ,
+                lastname: req.body.lastname,
+                password: hashedPass,
+                email: req.body.email
+            })
+            .then(() => res.status(201).send({ msg: 'You have succesfuly signed up'}))
+            .catch((err) => console.error(`User error: ${err}`))
         })
         .catch((hashErr) => console.error(`Hashing the password had the following errors: ${hashErr}`))
 
